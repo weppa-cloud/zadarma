3627c508b4e57177616fa55e417a9775f601cb48
Mauricio <mauroalternativo519@gmail.com>
2024-09-19 23:05:25 -0500

    Add scripts con Keys en php

--- index.php
+++ index.php
@@ -8,6 +8,35 @@ if (isset($_GET['zd_echo'])) exit($_GET['zd_echo']);
 <head>
     <meta charset="UTF-8">
     <title>Zadarma con PHP</title>
+
+    <!-- Agrega aquí los scripts de Zadarma -->
+    <script src="https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-lib.js?sub_v=1"></script>
+    <script src="https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-fn.js?sub_v=1"></script>
+    <script>
+        if (window.addEventListener) {
+            window.addEventListener('load', function() {
+                zadarmaWidgetFn(
+                    'ea0ff7c43545f6d0d97a', 
+                    '112571-102', 
+                    'square', 
+                    'es', 
+                    true, 
+                    {right: '10px', bottom: '5px'}
+                );
+            }, false);
+        } else if (window.attachEvent) {
+            window.attachEvent('onload', function() {
+                zadarmaWidgetFn(
+                    'ea0ff7c43545f6d0d97a', 
+                    '112571-102', 
+                    'square', 
+                    'es', 
+                    true, 
+                    {right: '10px', bottom: '5px'}
+                );
+            });
+        }
+    </script>
 </head>
 <body>
     <h1><?php echo "Hola desde PHP"; ?></h1>
@@ -16,3 +45,4 @@ if (isset($_GET['zd_echo'])) exit($_GET['zd_echo']);
     <script src="/js/scripts.js"></script>
 </body>
 </html>
+
