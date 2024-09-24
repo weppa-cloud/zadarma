533dc623aaa5921eb9a8b9010d4deb319f59917e
Mauricio <mauroalternativo519@gmail.com>
2024-09-24 14:06:53 -0500

    Debug scrip index

--- index.php
+++ index.php
@@ -15,6 +15,17 @@ if (isset($_GET['zd_echo'])) exit($_GET['zd_echo']);
     <!-- Cargar el widget de Zadarma -->
     <script src="https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-lib.js?sub_v=1"></script>
     <script src="https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-fn.js?sub_v=1"></script>
-    <script src="/scripts/phone-widget.js"></script>  <!-- Inicializar el widget con la clave WebRTC -->
+    <!-- <script src="/scripts/phone-widget.js"></script>  Inicializar el widget con la clave WebRTC -->
+    <script>
+        if (window.addEventListener) { 
+            window.addEventListener('load', function() { 
+            zadarmaWidgetFn('c69ec05795c4e3a004ae', '89075', 'square' /*square|rounded*/, 'en' /*ru, en, es, fr, de, pl, ua*/, true, "{right:'10px',bottom:'5px'}"); 
+            }, false); 
+        } else if (window.attachEvent) { 
+            window.attachEvent('onload', function(){ 
+            zadarmaWidgetFn('c69ec05795c4e3a004ae', '89075', 'square' /*square|rounded*/, 'en' /*ru, en, es, fr, de, pl, ua*/, true, "{right:'10px',bottom:'5px'}"); 
+            }); 
+        } 
+</script>
 </body>
 </html>
